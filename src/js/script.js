e52960df6b158d17c8cfe1a08fc26ef6c5ee6912
Kiwiguard <frida.susanne@gmail.com>
2020-10-21 16:23:24 +0200

    added js functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,36 @@
 // General scripts for entire webpage
+
+"use strict"
+
+//variables
+let courseEl = document.getElementById("courses");
+
+
+//eventhandlers
+window.addEventListener('load', getCourses);
+
+//functions
+function getCourses() {
+    courseEl.innerHTML = '';
+
+    fetch('http://localhost/moment5/api')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(course => {
+            //console.log(course);
+            courseEl.innerHTML +=
+            `<div class="course">
+                <p>
+                <b>Namn: </b> ${course.name}
+                <br>
+                <b>Kurskod: </b> ${course.code}
+                <br>
+                <b>Progression: </b> ${course.progression}
+                <br>
+                <a href=" ${course.syllabus}><b>Kursplan</b></a>
+                <button id="${course.id}" onClick="deleteCourse">Radera</button>
+            </div>`
+
+        })
+    })
+}
